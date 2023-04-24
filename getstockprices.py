35c06fd49ba7094f0ac4b46ad5f5d7de6ca75c12
Chris Pinto <Chris.pinto1234@gmail.com>
2023-04-24 14:56:12 -0230

    Added input for stock

--- getstockprices.py
+++ getstockprices.py
@@ -2,32 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-mystocks = ['TVE.TO', 'BNS.TO', 'BMO.TO', 'RY.TO']
-stockdata = []
+import requests
+from bs4 import BeautifulSoup
+import json
 
+symbol = input("Enter a stock symbol: ")
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'}
+url = f'https://ca.finance.yahoo.com/quote/{symbol}'
+r = requests.get(url, headers=headers)
 
-def getData(symbol):
-    headers= {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'}
-    url = f'https://ca.finance.yahoo.com/quote/{symbol}'
-    r = requests.get(url, headers=headers)
+if r.status_code == 200:
     soup = BeautifulSoup(r.text, 'html.parser')
     stock = {
-    'symbol' : symbol,
-    'price' : soup.find('div', {'class': 'D(ib) Mend(20px)'}).find_all('fin-streamer')[0].text,
-    'change' : soup.find('div', {'class': 'D(ib) Mend(20px)'}).find_all('fin-streamer')[1].text,
-    # 'summary': soup.find('div', {'class': 'businessSummary Mt(10px) Ov(h) Tov(e)'}).find_all('p')[1].text,
+        'symbol': symbol,
+        'price': soup.find('div', {'class': 'D(ib) Mend(20px)'}).find_all('fin-streamer')[0].text,
+        'change': soup.find('div', {'class': 'D(ib) Mend(20px)'}).find_all('fin-streamer')[1].text,
     }
-    return stock
+    with open('Stockdata.json', 'w') as f:
+        json.dump([stock], f)
+    print(f'Successfully scraped data for {symbol} and saved it to Stockdata.json')
+else:
+    print(f"Failed to get data for {symbol}. Please check the symbol and try again.")
 
-for item in mystocks:
-    stockdata.append(getData(item))
-    print("Getting:", item)
-
-with open('Stockdata.json', 'w') as f:
-    json.dump(stockdata, f)
-
-
-print('fin .')
 
 
 
